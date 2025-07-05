a565386fce332a37eb9a7126a2d1fa1487e48c35
josephkawabata <josephkawabata@gmail.com>
2025-07-04 19:31:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// No behavior needed for now, but the file is ready for expansion.
-console.log("Welcome to Joseph's site!");
+document.getElementById('project-list').innerHTML = `
+  <li><a href="https://josephkawabata.github.io/mathpractice/" target="_blank">Math Practice</a></li>
+  <li><a href="https://docs.google.com/spreadsheets/d/1mECb5aCkA_ar5_D8zKTuBmdPTSBsgu0Aiv0Pz6En9fo/edit?gid=0#gid=0" target="_blank">Dollar Cost Averaging Calculator</a></li>
+`;
